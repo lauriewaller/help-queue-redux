a355e19b0c901abadab46d9f5594f423f7e95115
Laurie Waller <lauriewaller7@gmail.com>
2021-08-11 20:30:16 -0700

    add delete button to TicketDetail

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -54,7 +54,7 @@ class TicketControl extends React.Component {
     let buttonText = null;
 
     if (this.state.selectedTicket != null) {
-      currentlyVisibleState = <TicketDetail ticket={this.state.selectedTicket} />
+      currentlyVisibleState = <TicketDetail ticket={this.state.selectedTicket} onClickingDelete={this.handleDeletingTicket} />
       buttonText = "Return to Ticket List";
       // While our TicketDetail component only takes placeholder data, we will eventually be passing the value of selectedTicket as a prop.
     }
