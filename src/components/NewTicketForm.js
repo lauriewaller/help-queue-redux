54013a4683215aef4d3dbf1f716e618509b1296a
Laurie Waller <lauriewaller7@gmail.com>
2021-08-11 20:41:15 -0700

    add reusable form

--- src/components/NewTicketForm.js
+++ src/components/NewTicketForm.js
@@ -1,28 +1,19 @@
 import React from "react";
 import { v4 } from 'uuid';
 import PropTypes from "prop-types"; 
+import ReusableForm from "./ReusableForm";
+
 
 function NewTicketForm(props){
 
   return (
     <React.Fragment>
-      <form onSubmit={handleNewTicketFormSubmission}> 
-      {/* onSubmit triggers when the submit button of a form is clicked */}
-        <input
-          type='text'
-          name='names'
-          placeholder='Pair Names' />
-        <input
-          type='text'
-          name='location'
-          placeholder='Location' />
-        <textarea
-          name='issue'
-          placeholder='Describe your issue.' />
-        <button type='submit'>Help!</button>
-      </form>
+      <ReusableForm 
+        formSubmissionHandler={handleNewTicketFormSubmission}
+        buttonText="Help!" />
     </React.Fragment>
   );
+  
   function handleNewTicketFormSubmission(event) {
     event.preventDefault();
     props.onNewTicketCreation({names: event.target.names.value, location: event.target.location.value, issue: event.target.issue.value, id: v4()});
