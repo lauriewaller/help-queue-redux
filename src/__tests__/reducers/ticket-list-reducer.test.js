df63e3547ad82055ca01b914b09e0628e86fb987
Laurie Waller <lauriewaller7@gmail.com>
2021-08-22 12:03:11 -0700

    write add_ticket test

--- src/__tests__/reducers/ticket-list-reducer.test.js
+++ src/__tests__/reducers/ticket-list-reducer.test.js
@@ -9,8 +9,29 @@ describe('ticketListReducer', () => {
     issue: 'Redux action is not working correctly.',
     id: 1
   };
-  
+
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(ticketListReducer({}, { type: null })).toEqual({});
   });
+
+  test('Should successfully add new ticket data to masterTicketList', () => {
+    const { names, location, issue, id } = ticketData;
+    action = {
+      type: 'ADD_TICKET',
+      names: names,
+      location: location,
+      issue: issue,
+      id: id
+    };
+
+    expect(ticketListReducer({}, action)).toEqual({
+      [id] : {
+        names: names,
+        location: location,
+        issue: issue,
+        id: id
+      }
+    });
+  });
+  
 });
