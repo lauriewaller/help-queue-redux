e9b3a86917423f29cf3da561b5cd695b76800a49
Laurie Waller <lauriewaller7@gmail.com>
2021-09-01 08:26:08 -0700

    update reducer

--- src/reducers/ticket-list-reducer.js
+++ src/reducers/ticket-list-reducer.js
@@ -14,15 +14,17 @@ export default (state = {}, action) => {
           formattedWaitTime: formattedWaitTime,
         },
       });
+    //timeOpen will store when a ticket was opened while formattedWaitTime will store a string with user-friendly elapsed wait time (such as "2 minutes ago").
     case c.DELETE_TICKET:
       let newState = { ...state };
       delete newState[id];
       return newState;
     case c.UPDATE_TIME:
-      const newTicket = Object.assign({}, state[id], { formattedWaitTime });
+      const newTicket = Object.assign({}, state[id], { formattedWaitTime }); //?
       const updatedState = Object.assign({}, state, {
         [id]: newTicket,
       });
+      //Since the updatedTicket's id already exists in the copy of the ticket list, the old ticket will be replaced with the updated ticket.
       return updatedState;
     default:
       return state;
